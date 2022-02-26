103e09aecf92568368420b2e438915128abe747a
jspoh <99598950+jspoh@users.noreply.github.com>
2022-02-26 16:05:11 +0800

    Update and rename auto_typer.py to auto_clicker_typer.py
    added autoclicker

--- auto_clicker_typer.py
+++ auto_clicker_typer.py
@@ -1,6 +1,8 @@
-# auto typing bot. can help with filling up forms that require eg. email addresses etc
+# auto typing + auto clicking bot. can help with filling up forms that require eg. email addresses etc
 
 import keyboard
+import mouse
+import time
 
 start_stop_key = keyboard.KEY_DOWN
 
@@ -33,8 +35,8 @@ Auto Typer v2 will record all keystrokes at the speed and sequence you type it a
 including pauses, mistakes etc.
 
 ''')
-        print("Enter the text you desire to autotype, press [Escape] when you're done.\n")
-        auto_text_v2 = keyboard.record(until="esc")
+        print("Enter the text you desire to autotype, press [Enter] when you're done.\n")
+        auto_text_v2 = keyboard.record(until="Enter")
         input()  # to prevent the program from going straight to input in the next line
         type_speed = input('''
 How fast would you like the bot to type your text?
@@ -51,12 +53,44 @@ Enter '0' for instant, '1' for 1x, '2' for 2x etc..
                 a_type_v2 = False
 
 
+    def auto_click():
+        print('''
+Press [arrow down] to start/stop clicking the left mouse button.
+This program does ~30 clicks per second
+''')
+        #cps = float(input("Clicks per second: "))
+        autoclick = True
+        while autoclick:
+            if keyboard.is_pressed(start_stop_key):
+                print("Auto click starting in 3 seconds")
+                time.sleep(3)
+                print("Auto click started!")
+                aclick_running = True
+                while aclick_running:
+                    mouse.click("left")
+                    time.sleep(0.02)  # ~30cps
+                    if keyboard.is_pressed(start_stop_key):
+                        print("Autoclick stopped!")
+                        aclick_running = False
+                        time.sleep(0.5)
+
+
+
+    def test():
+        while True:
+            if keyboard.is_pressed(start_stop_key):
+                print("lol")
+                time.sleep(0.5)
+
+
+
     def intro_text():
         func = input('''
 Which function would you like to use?
         
 1. AutoTyper
 2. AutoTyper v2
+3. Auto clicker
 
 Hotkey to start/stop is [arrow down]
 Alternatively, enter 'Q' to quit program.
@@ -70,7 +104,10 @@ Alternatively, enter 'Q' to quit program.
             auto_type()
         elif func == "2":
             auto_type_v2()
+        elif func == "3":
+            auto_click()
+        elif func == "0":
+            test()
 
 
     intro_text()
-

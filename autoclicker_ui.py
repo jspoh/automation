a64bfababb48a7bd4cfbc18ff00c4238950958b0
jspoh <99598950+jspoh@users.noreply.github.com>
2022-03-07 02:31:24 +0800

    fixed hotkeys
    program seems to not like it when the autoclick target is its own window.
    cant get program to respond to the starting hotkey when the window is not selected either, even with the keyboard module which worked fine before

--- autoclicker_ui.py
+++ autoclicker_ui.py
@@ -1,6 +1,7 @@
 # autoclicker with pyautogui instead of mouse
 # made a gui for this but starting with hotkey is broken now :(
-# everything else seems to work fine though, can't really test stop button for some reason(program stops responding)
+# everything else seems to work fine though, can't really test stop button though
+# program crashes when it starts clicking on the window
 
 import keyboard
 import pyautogui
@@ -11,24 +12,29 @@ sg.theme("DarkAmber")
 
 layout_size = (40, 1)
 
+font = "Arial"
+font_size = 12
+
 layout = [
-    [sg.Text("Autoclicker by JS", size=layout_size)],
+    [sg.Text("Autoclicker by JS", size=(20, 1), font=(font, 20))],
     [sg.Text("")],
-    [sg.Text("Select your desired speed mode below", size=layout_size)],
+    [sg.Text("Select your desired speed mode below", size=layout_size, font=(font, font_size))],
     [sg.Radio("Slow", group_id="speed", key="slow", default=False, enable_events=True),
      sg.Radio("Fast", group_id="speed", key="fast", default=True, enable_events=True),
      sg.Radio("Faster", group_id="speed", key="faster", default=False, enable_events=True),
      sg.Radio("Fastest", group_id="speed", key="fastest", default=False, enable_events=True)],
     [sg.Text("")],
-    [sg.Button("Start", key="start"), sg.Button("Stop(F8)", key="stop")]
+    [sg.Button("Start(F8)", key="start"), sg.Button("Stop(F8)", key="stop")],
+    [sg.Text("Note: Start hotkey will only work when this window is selected!", font=("Arial", 7))]
 ]
 
-window = sg.Window("Autoclicker by JS", layout)
+window = sg.Window("Autoclicker by JS", layout, return_keyboard_events=True)
 
 pyautogui.PAUSE = 0.0001
 
-hotkey = "F8"
-q_hotkey = "F7"
+hotkey = "F8:119"
+k_hotkey = "F8"
+q_hotkey = "F7:118"
 
 
 def click0():
@@ -58,28 +64,28 @@ running = True
 while running:
     event, values = window.read()
     print(event, values)
-    if event == sg.WIN_CLOSED or keyboard.is_pressed(q_hotkey):
+    if event == sg.WIN_CLOSED:
         running = False
-    elif keyboard.is_pressed(hotkey) or event == "start":  # hotkey to start isn't working for some reason
+    elif keyboard.is_pressed(k_hotkey) or event == hotkey or event == "start":
         time.sleep(0.5)
         print("autoclick started!")
         while values["slow"]:
             click0()
-            if keyboard.is_pressed(hotkey) or event == "stop":
+            if keyboard.is_pressed(k_hotkey) or event == "stop":
                 stop_click()
                 break
         while values["fast"]:
             click1()
-            if keyboard.is_pressed(hotkey) or event == "stop":
+            if keyboard.is_pressed(k_hotkey) or event == "stop":
                 stop_click()
                 break
         while values["faster"]:
             click2()
-            if keyboard.is_pressed(hotkey) or event == "stop":
+            if keyboard.is_pressed(k_hotkey) or event == "stop":
                 stop_click()
                 break
         while values["fastest"]:
             click3()
-            if keyboard.is_pressed(hotkey) or event == "stop":
+            if keyboard.is_pressed(k_hotkey) or event == "stop":
                 stop_click()
                 break
